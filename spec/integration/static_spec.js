19f4ff9927322d93db1076eecccda48ff8a33998
Zachary Guerrin <39317224+zguerrin27@users.noreply.github.com>
2019-06-15 13:57:43 -0400

    Assignment-2: Completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marco = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
 
@@ -11,7 +12,16 @@ describe("routes : static", () => {
         done();
       });
     });
+  }); 
 
+  describe("GET /marco", () => {
+    it("should return status code of 200 and contain polo in the body", (done) => {
+      request.get(marco, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("polo");
+        done();
+      });
+    });
   });
 
 }); // Main Routes 
