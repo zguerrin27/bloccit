26d4e7cb43eff101de68516303bdcb6057375582
Zachary Guerrin <39317224+zguerrin27@users.noreply.github.com>
2019-06-30 12:49:45 -0400

    Checkpoint-10: Completed

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -76,6 +76,29 @@ describe("routes : posts", () => {
        );
      });
 
+    it("should not create a new post that fails validations", (done) => {
+      const options = {
+        url: `${base}/${this.topic.id}/posts/create`,
+        form: {
+          title: "a",
+          body: "b"
+        }
+      };
+      request.post(options,
+        (err, res, body) => {
+          Post.findOne({where: {title: "a"}})
+          .then((post) => {
+              expect(post).toBeNull();
+              done();
+          })
+          .catch((err) => {
+            console.log(err);
+            done();
+          });
+        }
+      );
+    });
+
   });
 
   describe("GET /topics/:topicId/posts/:id", () => {
@@ -138,7 +161,8 @@ describe("routes : posts", () => {
         const options = {
           url: `${base}/${this.topic.id}/posts/${this.post.id}/update`,
           form: {
-            title: "Snowman Building Competition"
+            title: "Snowman Building Competition",
+            body: "I really enjoy the funny hats on them."
           }
         };
         request.post(options,
