0fb2d2a02b87fde75d9be2eb0957f3f0bf9e5d47
Zachary Guerrin <39317224+zguerrin27@users.noreply.github.com>
2019-07-02 12:14:20 -0400

    Checkpoint-11: Completed..Pushing to Production

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -5,11 +5,13 @@ module.exports = {
     const advertRoutes = require("../routes/adverts");
     const postRoutes = require("../routes/posts");
     const flairRoutes = require("../routes/flairs");
+    const userRoutes = require("../routes/users");
 
     app.use(staticRoutes);
     app.use(topicRoutes);
     app.use(advertRoutes);
     app.use(postRoutes);
     app.use(flairRoutes);
+    app.use(userRoutes);
   }
 }
